cf18e713a738308582ab5b3c9ec2138df47e1ed2
CarolinaCl <nodabolivia.com>
2022-01-18 17:14:54 -0400

    Reto1

--- figuras.js
+++ figuras.js
@@ -86,3 +86,28 @@ function calcularAreaCirculo() {
     const area = areaCirculo(value);
     alert(area);
 }
+
+
+/*TALLER RETO
+En este ejercicio debes crear una función para calcular la altura de un triángulo isósceles.
+
+La función debe recibir, como parámetros, la longitud de los 3 lados del triángulo.
+La función debe validar que la longitud de los 3 lados del triángulo corresponden a un triángulo isósceles.
+La función debe retornar la altura del triángulo.
+*/
+function alturaTrianguloIsosceles(trianguloGrandeLadoA, trianguloGrandeLadoB, trianguloGrandeLadoBase) {
+    if (trianguloGrandeLadoA != trianguloGrandeLadoB) {
+        console.error("Los lados a y b no son iguales");
+    } else {
+        const trianguloPequenoLadoB = trianguloGrandeLadoBase / 2;
+        const trianguloPequenoLadoBase = trianguloGrandeLadoA;
+
+        const trianguloPequenoLadoBCuadrado = trianguloPequenoLadoB * trianguloPequenoLadoB;
+        const trianguloPequenoLadoBaseCuadrado = trianguloPequenoLadoBase * trianguloPequenoLadoBase;
+
+        const trianguloPequenoLadoA = Math.sqrt(trianguloPequenoLadoBaseCuadrado - trianguloPequenoLadoBCuadrado);
+
+        const trianguloGrandeAltura = trianguloPequenoLadoA;
+        return trianguloGrandeAltura;
+    }
+}
