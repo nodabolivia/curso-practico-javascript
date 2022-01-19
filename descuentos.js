2c288d94e9813c510228322318efe221be1b854a
CarolinaCl <nodabolivia.com>
2022-01-18 21:14:58 -0400

    reto modulo2 1era opcion con switch

--- descuentos.js
+++ descuentos.js
@@ -21,15 +21,32 @@ porcentajePrecioConDescuento: 85
 precioConDescuento: 85
 precioOriginal: 100
 } */
+const coupons = [
+    "batman",
+    "nadie",
+    "secreto",
+];
 function onClickButtonPriceDiscount() {
     const inputPrice = document.getElementById("price");
     const priceValue = inputPrice.value;
-    const inputDiscount = document.getElementById("discount");
-    const discountValue = inputDiscount.value;
-
-    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+    const inputCoupon = document.getElementById("coupon");
+    const couponValue = inputCoupon.value;
+    let descuento;
+    switch (couponValue) {
+        case coupons[0]:
+            descuento = 15;
+            break;
+        case coupons[1]:
+            descuento = 30;
+            break;
+        case coupons[2]:
+            descuento = 25;
+            break;
+    }
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
 
     const resultP = document.getElementById("resultPrice");
     resultP.innerText = "El precio con descuento son $" + precioConDescuento;
 
 }
+
