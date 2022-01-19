dac3ab53fa5843ef986ac231cd2ca39db26a4028
CarolinaCl <nodabolivia.com>
2022-01-18 22:28:17 -0400

    solucion con for clasico

--- promedio.js
+++ promedio.js
@@ -1,13 +1,10 @@
 // 1 mediana
-const lista1 = [
-    100,
-    200,
-    300,
-    500,
-];
 
-let sumaLista1 = 0;
-for (let i = 0; i < lista1.length; i++) {
-    sumaLista1 = sumaLista1 + lista1[i];
+function calcularMediaArimetica(lista) {
+    let sumaLista = 0;
+    for (let i = 0; i < lista.length; i++) {
+        sumaLista = sumaLista + lista[i];
+    }
+    const promedioLista = sumaLista / lista.length;
+    return promedioLista;
 }
-const promedioLista1 = sumaLista1 / lista1.length;
